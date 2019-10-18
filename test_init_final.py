6160238e59884efd566242edef3536627f69e205
bossbot6sub <51066630+bossbot6sub@users.noreply.github.com>
2019-10-19 07:04:52 +0900

    Update test_init_final.py

--- test_init_final.py
+++ test_init_final.py
@@ -160,8 +160,8 @@ def init():
 	#print (inputData, len(inputData))
 	
 	### 강제 채널 고정###
-	basicSetting[6] = int('570879726256652311') #보이스채널ID
-	basicSetting[7] = int('617969126161383424') #택스트채널ID
+	basicSetting[6] = int('632845051592048653') #보이스채널ID
+	basicSetting[7] = int('632844999054065684') #택스트채널ID
 	
 	bossNum = int(len(boss_inputData)/5)
 
